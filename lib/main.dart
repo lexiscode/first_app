bda49297cf95223677de4d13e09253ee39604110
Nwokorie Alex V <nwokoriealex20@gmail.com>
2024-08-11 21:17:00 +0100

    feat: using columns in the scaffold body

--- lib/main.dart
+++ lib/main.dart
@@ -12,21 +12,42 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.blue,
-        appBar: AppBar(
-          backgroundColor: Colors.black,
-          //foregroundColor: const Color(0xffFFFFFF),
-          centerTitle: true,
-          //elevation: 0.0,
-          title: const Text(
-            "My Dashatar App",
-            style: TextStyle(color: Color(0xffFFFFFF), fontSize: 30.0),
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            backgroundColor: Colors.black,
+            centerTitle: true,
+            title: const Text(
+              "My Dashatar App",
+              style: TextStyle(color: Color(0xffFFFFFF), fontSize: 30.0),
+            ),
           ),
-        ),
-        body: Center(
-          child: Image.asset('images/dashatar.png'),
-        ),
-      ),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Container(
+                  height: 100.0,
+                  width: 100.0,
+                  color: Colors.yellow,
+                  child: Image.asset(
+                    'images/dashatar.png',
+                  )),
+              Container(
+                  height: 100.0,
+                  width: 200.0,
+                  color: Colors.black,
+                  child: Image.asset(
+                    'images/dashatar.png',
+                  )),
+              Container(
+                  height: 100.0,
+                  width: 100.0,
+                  color: Colors.red,
+                  child: Image.asset(
+                    'images/dashatar.png',
+                  )),
+            ],
+          )),
     );
   }
 }
